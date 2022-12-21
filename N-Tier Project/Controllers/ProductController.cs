a7dc8a1f448720bbf7199bf00e92abe5f5ff378f
Sarkhan Abbaszadeh <s.abbaszada@ady.az>
2022-12-21 17:37:57 +0400

    add uiLayout

---
+++ N-Tier Project/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace N_Tier_Project.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
