a6998090c840e6446b41cba05eac4e7e7ec58e44
Sarkhan Abbaszadeh <s.abbaszada@ady.az>
2022-12-21 15:09:18 +0400

    Add project files.

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,27 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=SABBASZADA-N\\MSSQLSERVER2;database=DbNTierCore;integrated security=true;encrypt=false");
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+    }
+
+}
