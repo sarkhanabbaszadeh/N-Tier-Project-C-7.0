b3a86393ad294de27594ea736eb79fbacfdb6d6a
Sarkhan Abbaszadeh <s.abbaszada@ady.az>
2022-12-21 16:06:17 +0400

    add business layer services,valitdations

---
+++ BusinessLayer/FluentValidation/ProductValidator.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.FluentValidation
+{
+    public class ProductValidator : AbstractValidator<Product>
+    {
+        public ProductValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Məhsul adı boş buraxıla bilməz!");
+            RuleFor(x => x.Name).MinimumLength(3).WithMessage("Məhsul adı ən azı 3 hərfdən ibarət olmalıdır!");
+            RuleFor(x => x.Price).NotEmpty().WithMessage("Qiymət boş buraxıla bilməz!");
+            RuleFor(x => x.Stock).NotEmpty().WithMessage("Miqdar sayı boş buraxıla bilməz");
+        }
+    }
+}
