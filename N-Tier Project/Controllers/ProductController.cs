bed1f2fbe81d09bd566cf9f12e2af3faa6f1ea5d
Sarkhan Abbaszadeh <s.abbaszada@ady.az>
2022-12-22 17:43:08 +0400

    add Validation

--- N-Tier Project/Controllers/ProductController.cs
+++ N-Tier Project/Controllers/ProductController.cs
@@ -1,11 +1,44 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.FluentValidation;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
 
 namespace N_Tier_Project.Controllers
 {
     public class ProductController : Controller
     {
+        ProductManager productManager = new ProductManager(new EfProductDal());
         public IActionResult Index()
         {
+            var values = productManager.TGetList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult AddProduct()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddProduct(Product p)
+        {
+            ProductValidator validationRules= new ProductValidator();
+            ValidationResult results=validationRules.Validate(p);
+            if(results.IsValid)
+            {
+                productManager.TInsert(p);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach(var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
             return View();
         }
     }
