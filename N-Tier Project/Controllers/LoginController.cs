30e1ac946b4334db4426a070eaea81bcae51564b
Sarkhan Abbaszadeh <s.abbaszada@ady.az>
2022-12-26 16:47:12 +0400

    add Login

---
+++ N-Tier Project/Controllers/LoginController.cs
@@ -0,0 +1,44 @@
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using N_Tier_Project.Models;
+using System.Drawing.Text;
+
+namespace N_Tier_Project.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly SignInManager<AppUser> _signManager;
+
+        public LoginController(SignInManager<AppUser> signManager)
+        {
+            _signManager = signManager;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(UserLoginViewModel uslm)
+        {
+            if(ModelState.IsValid)
+            {
+                var result = await _signManager.PasswordSignInAsync(uslm.UserName, uslm.Password, false, true);
+
+                if(result.Succeeded)
+                {
+                    return RedirectToAction("Index","Category");
+                }
+
+                else
+                {
+                    ModelState.AddModelError("", "Xəta ! Yalnış istifadəçi adı vəya şifrə");
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ N-Tier Project/Models/UserLoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace N_Tier_Project.Models
+{
+    public class UserLoginViewModel
+    {
+        [Required(ErrorMessage ="Zəhmət olmasa istifadəçi adınızı daxil edin")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage ="Zəhmət olmasa şifrəni daxil edin")]
+        public string Password { get; set; }
+    }
+}
